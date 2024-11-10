7fb699be038e408168cfcba1d31df587fb3093a5
Md Aminul Islam <mr.aminul118@gmail.com>
2024-11-10 19:17:54 +0600

    Register page design

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,15 +1,25 @@
+import { Link } from "react-router-dom";
+
 const Login = () => {
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(email, password);
+  };
+
   return (
     <div className="flex justify-center items-center min-h-[calc(100vh-292px)] ">
-      <div className="card bg-base-100 w-full max-w-sm shrink-0 ">
-        <form className="card-body">
-          <h2 className="text-3xl font-bold py-8"> Login</h2>
+      <div className="card p-8 shadow-2xl w-full max-w-lg shrink-0 space-y-2 ">
+        <h2 className="text-3xl font-bold py-8"> Login</h2>
+        <form onSubmit={handleLogin}>
           <div className="form-control">
             <label className="label">
               <span className="label-text">Email</span>
             </label>
             <input
               type="email"
+              name="email"
               placeholder="email"
               className="input input-bordered"
               required
@@ -21,6 +31,7 @@ const Login = () => {
             </label>
             <input
               type="password"
+              name="password"
               placeholder="password"
               className="input input-bordered"
               required
@@ -35,6 +46,12 @@ const Login = () => {
             <button className="btn btn-primary">Login</button>
           </div>
         </form>
+        <p className="py-4">
+          New to this website? please
+          <Link className="text-blue-500 hover:underline ml-2" to={`/register`}>
+            Register
+          </Link>
+        </p>
       </div>
     </div>
   );
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,10 +1,70 @@
+import { Link } from "react-router-dom";
 
 const Register = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const handleRegister = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(name, email, password);
+  };
+
+  return (
+    <div className="flex justify-center items-center min-h-[calc(100vh-292px)] ">
+      <div className="card p-8 shadow-2xl w-full max-w-lg shrink-0 space-y-2 ">
+        <h2 className="text-3xl font-bold py-8"> Register</h2>
+        <form onSubmit={handleRegister}>
+          {/* Name */}
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Name</span>
+            </label>
+            <input
+              type="text"
+              name="name"
+              placeholder="name"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          {/* Email */}
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              name="email"
+              placeholder="email"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              name="password"
+              placeholder="password"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn btn-primary">Register</button>
+          </div>
+        </form>
+        <p className="py-4">
+          Already have an account? please
+          <Link className="text-blue-500 hover:underline ml-2" to={`/login`}>
+            Login
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
